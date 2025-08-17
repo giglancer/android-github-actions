887366eb9e77f16a7abb59b7d8e0015a341aef40
giglancer <72249208+giglancer@users.noreply.github.com>
2025-08-17 10:08:33 +0000

    Release v1.0.6 (build 24)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.example.androidsampleactions"
         minSdk = 24
         targetSdk = 36
-        versionCode = 23
-        versionName = "1.0.5"
+        versionCode = 24
+        versionName = "1.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
